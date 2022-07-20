4ecf8b6818ff64d0ae610a99d0c1744ae8811b75
theidriselijah <iearmstrong@icloud.com>
2022-07-20 16:11:48 -0700

    Fetch API Request Practice

---
+++ user.js
@@ -0,0 +1,28 @@
+const postListEl = document.querySelector(' .post-list');
+const id = localStorage.getItem("id");
+
+
+async function onSearchChange(event) {
+    const id = event.target.value;
+    renderPost(id);
+}
+
+async function renderPost(userId) {
+    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId || id}`);
+    const postsData = await posts.json();
+    postListEl.innerHTML = postsData.map(post => postHTML(post)).join('');
+}
+
+function postHTML(post) {
+    return `<div class="post">
+        <div class="post__title">
+         ${post.title}
+        </div>
+        <p class="post__body">
+        ${post.body}
+        </p>
+    </div>
+  `
+}
+
+renderPost(id);
